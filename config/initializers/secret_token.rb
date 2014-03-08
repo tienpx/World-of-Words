b78e9cb660a62829cfaf7020e1f50f19f9860c0e
tienpx <tienpx_57@vnu.edu.vn>
2014-03-09 02:18:59 +0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WorldOfWords::Application.config.secret_key_base = '782790a334a790e98b4cef25f2bff9afd668d24f5a8895a5c0da11f901086a3b348306347d63315de1fb1ff274be99356af3bd1c5c5a2dffe1a82f5b0a077250'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+WorldOfWords::Application.config.session_store :cookie_store, key: '_World-of-Words_session'
